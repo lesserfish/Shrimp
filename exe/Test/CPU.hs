517690b80c3f5b63e4bfab23abb689f91cba8558
lesserfish <lesserfish@pm.me>
2023-08-27 12:07:36 -0300

    CPU Passes 00.json

--- exe/Test/CPU.hs
+++ exe/Test/CPU.hs
@@ -70,19 +70,43 @@ loadCPU (CPUState rpc rs ra rx ry rp _) = MOS6502 reg 0 0
   where
     reg = Registers rpc rs ra rx ry rp
 
-loadBarebones :: CPUState -> Barebones
-loadBarebones cpustate = Barebones (loadCPU cpustate) (loadMemory . ram $ cpustate) []
+bLoad :: CPUState -> Barebones
+bLoad cpustate = Barebones (loadCPU cpustate) (loadMemory . ram $ cpustate) []
 
 join :: (MOS6502, Barebones) -> Barebones
 join (cpu, barebones) = barebones{bCpu = cpu}
 
-tickBarebones :: Barebones -> Barebones
-tickBarebones barebones = barebones'
+bTick :: Barebones -> Barebones
+bTick barebones = barebones'
   where
     barebones' = join result
     result = execState tick (bCpu barebones, barebones)
 
-(.**.) :: (a -> a) -> Int -> (a -> a)
-(.**.) f n x
-    | n == 1 = f x
-    | otherwise = f . (f .**. (n - 1)) $ x
+verifyRam :: Array Word16 Word8 -> [(Word16, Word8)] -> Bool
+verifyRam array [] = True
+verifyRam array (y : ys) = this && that
+  where
+    (addr, byte) = y
+    byte' = array ! addr
+    this = byte == byte'
+    that = verifyRam array ys
+
+verifyCPU :: MOS6502 -> CPUState -> Bool
+verifyCPU mos6502 cpustate = condition
+  where
+    pc_condition = (Shrimp.MOS6502.pc . mosRegisters $ mos6502) == (Test.CPU.pc cpustate) -- Tests whether PC register is the same
+    sp_condition = (sp . mosRegisters $ mos6502) == (s cpustate) -- Tests whether SP register is the same
+    acc_condition = (acc . mosRegisters $ mos6502) == (a cpustate) -- Tests whether Accumulator register is the same
+    x_condition = (idx . mosRegisters $ mos6502) == (x cpustate) -- Tests whether X register is the same
+    y_condition = (idy . mosRegisters $ mos6502) == (y cpustate) -- Tests whether Y register is the same
+    ps_condition = (ps . mosRegisters $ mos6502) == (p cpustate) -- Tests whether PS register is the same
+    condition =
+        pc_condition
+            && sp_condition
+            && acc_condition
+            && x_condition
+            && y_condition
+            && ps_condition
+
+verify :: Barebones -> CPUState -> Bool
+verify barebones final_state = (verifyRam (bRam barebones) (ram final_state)) && (verifyCPU (bCpu barebones) final_state)
--- exe/Test/Main.hs
+++ exe/Test/Main.hs
@@ -15,8 +15,28 @@ loadJSON filePath = do
         Left err_msg -> error ("Could not parse JSON file to Tests data: " ++ err_msg)
         Right output -> return output
 
+test :: Test -> Bool
+test t = status
+  where
+    init_state = initial t
+    final_state = final t
+    barebones = bLoad init_state
+    barebones' = bTick barebones
+    status = verify barebones' final_state
+
+hTest' :: Test -> Spec
+hTest' t = describe "MOS6502 CPU Test" $ do
+    let n = name t
+    it (show n) $ do
+        test t `shouldBe` True
+
+hTest :: [Test] -> IO ()
+hTest [] = return ()
+hTest (y : ys) = do
+    hspec $ hTest' y
+    hTest ys
+
 main :: IO ()
 main = do
-    json <- loadJSON "/home/lesserfish/Documents/Code/Shrimp/exe/Test/TomHarte/6502/v1/00.json"
-    putStrLn $ show (json !! 0)
-    return ()
+    tests <- loadJSON "/home/lesserfish/Documents/Code/Shrimp/exe/Test/TomHarte/6502/v1/00.json"
+    hTest tests
