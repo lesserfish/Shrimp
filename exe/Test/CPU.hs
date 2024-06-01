c3590f9c02d32013052b60809959abc0321b821c
lesserfish <lesserfish@pm.me>
2024-06-01 09:00:03 -0300

    CPU tests are passing again
    Rewrote the tests to match the new API. Everything passing!

--- exe/Test/CPU.hs
+++ exe/Test/CPU.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 module Test.CPU where
 
+import System.IO.Unsafe
 import Control.Monad.State
 import Data.Aeson (FromJSON)
 import Data.Array
 import Data.Word
 import GHC.Generics
-import Shrimp.AbstractBus
 import Shrimp.MOS6502
+import qualified Shrimp.Memory as M
 import Text.Printf
 
 data CPUState = CPUState
@@ -33,137 +35,81 @@ data Test = Test
 
 data Barebones = Barebones
     { bCpu :: MOS6502
-    , bRam :: Array Word16 Word8
+    , bRam :: M.RAM
     , bLog :: [(Word16, Word8, String)]
     }
 
-showMem :: Array Word16 Word8 -> String
-showMem mem = showRam (filter (\(_, x) -> x /= 0) (assocs mem))
-
-showRam' :: [(Word16, Word8)] -> String
-showRam' [] = ""
-showRam' (y : ys) = (printf "(%04x , %02x)" addr value) ++ rest
-  where
-    (addr, value) = y
-    rest = showRam' ys
-
-showRam :: [(Word16, Word8)] -> String
-showRam y = "[" ++ showRam' y ++ "]"
-
-showLog' :: [(Word16, Word8, String)] -> String
-showLog' [] = ""
-showLog' (y : ys) = (printf "(%04x , %02x, " addr value) ++ str ++ ")" ++ rest
-  where
-    (addr, value, str) = y
-    rest = showLog' ys
-
-showLog :: [(Word16, Word8, String)] -> String
-showLog y = "[" ++ showLog' y ++ "]"
-
-showWord16 :: Word16 -> String
-showWord16 word = show word ++ (printf "\t(0x%04x)" word)
-
-showWord8 :: Word8 -> String
-showWord8 word = show word ++ (printf "\t(0x%02x)\t(%08b)" word word)
-
 instance Show CPUState where
-    show cpustate =
-        "CPU:"
-            ++ "\npc: \t"
-            ++ showWord16 (Test.CPU.pc cpustate)
-            ++ "\ns: \t"
-            ++ showWord8 (s cpustate)
-            ++ "\na: \t"
-            ++ showWord8 (a cpustate)
-            ++ "\nx: \t"
-            ++ showWord8 (x cpustate)
-            ++ "\ny: \t"
-            ++ showWord8 (y cpustate)
-            ++ "\np: \t"
-            ++ showWord8 (p cpustate)
-            ++ "\n"
-            ++ (showRam . ram $ cpustate)
-            ++ "\n"
-
-instance Show Barebones where
-    show barebones =
-        show (bCpu barebones)
-            ++ "\nLog: "
-            ++ showLog (bLog barebones)
-            ++ "\nRam: "
-            ++ showMem (bRam barebones)
-            ++ "\nCycles: "
-            ++ showLog (bLog $ barebones)
+    show cpustate = "YOU HAVE TO IMPLEMENT SHOW."
 
 pushLog :: (Word16, Word8, String) -> Barebones -> Barebones
 pushLog info barebones = barebones{bLog = (bLog barebones) ++ [info]}
 
-instance PCBus Barebones where
-    pcReadByte addr barebones = (barebones', byte)
-      where
-        byte = (bRam barebones) ! addr
-        barebones' = pushLog (addr, byte, "read") barebones
-    pcWriteByte addr byte barebones = barebones''
-      where
-        ram' = (bRam barebones) // [(addr, byte)]
-        barebones' = barebones{bRam = ram'}
-        barebones'' = pushLog (addr, byte, "write") barebones'
-    pcPeek addr barebones = byte
-      where
-        byte = (bRam barebones) ! addr
 
-emptyArray :: Int -> Array Word16 Word8
-emptyArray size = listArray (0, fromIntegral size - 1) (replicate (fromIntegral size) 0)
+cpuInterface :: M.RAM -> Interface
+cpuInterface ram = Interface readByte writeByte peekByte where
+    readByte addr = M.readByte ram addr
+    writeByte addr byte = M.writeByte ram addr byte
+    peekByte = readByte
 
-loadMemory :: [(Word16, Word8)] -> Array Word16 Word8
-loadMemory [] = emptyArray (1024 * 64)
-loadMemory (y : ys) = memory // memdata
-  where
-    memory = loadMemory ys
-    memdata = [y]
+loadMemory :: [(Word16, Word8)] -> IO M.RAM
+loadMemory [] = M.new (1024 * 64) 0
+loadMemory (y : ys) = do
+    m <- loadMemory ys
+    let (addr, byte) = y
+    M.writeByte m addr byte
+    return m
 
-loadCPU :: CPUState -> MOS6502
-loadCPU (CPUState rpc rs ra rx ry rp _) = MOS6502 reg 0 0 (Context False)
-  where
-    reg = Registers rpc rs ra rx ry rp
+loadCPU :: Interface -> CPUState -> MOS6502
+loadCPU interface (CPUState rpc rs ra rx ry rp _) = mos{registers = reg} where
+    mos = new interface
+    reg = Registers{
+                  Shrimp.MOS6502.pc = rpc,
+                  sp = rs,
+                  acc = ra,
+                  idx = rx,
+                  idy = ry,
+                  ps = rp
+              }
 
-bLoad :: CPUState -> Barebones
-bLoad cpustate = Barebones (loadCPU cpustate) (loadMemory . ram $ cpustate) []
+bLoad :: CPUState -> IO Barebones
+bLoad cpustate = do
+    ram <- loadMemory $ ram cpustate
+    let interface = cpuInterface ram
+    let cpu = loadCPU interface cpustate
+    return $ Barebones cpu ram []
 
-join :: (MOS6502, Barebones) -> Barebones
-join (cpu, barebones) = barebones{bCpu = cpu}
+bTick :: Barebones -> IO Barebones
+bTick barebones = do
+    mos' <- execStateT tick (bCpu barebones)
+    return barebones{bCpu = mos'}
 
-bTick :: Barebones -> Barebones
-bTick barebones = barebones'
-  where
-    barebones' = join result
-    result = execState tick (bCpu barebones, barebones)
-
-verifyRam :: Array Word16 Word8 -> [(Word16, Word8)] -> Bool
-verifyRam array [] = True
-verifyRam array (y : ys) = this && that
-  where
-    (addr, byte) = y
-    byte' = array ! addr
-    this = byte == byte'
-    that = verifyRam array ys
+verifyRam :: M.RAM -> [(Word16, Word8)] -> IO Bool
+verifyRam ram [] = return $ True
+verifyRam ram (y : ys) = do
+    that <- verifyRam ram ys
+    let (addr, byte) = y
+    byte' <- M.readByte ram addr 
+    let this = byte == byte'
+    return $ this && that
 
 verifyCPU :: MOS6502 -> CPUState -> Bool
-verifyCPU mos6502 cpustate = condition
-  where
-    pc_condition = (Shrimp.MOS6502.pc . registers $ mos6502) == (Test.CPU.pc cpustate) -- Tests whether PC register is the same
-    sp_condition = (sp . registers $ mos6502) == (s cpustate) -- Tests whether SP register is the same
-    acc_condition = (acc . registers $ mos6502) == (a cpustate) -- Tests whether Accumulator register is the same
-    x_condition = (idx . registers $ mos6502) == (x cpustate) -- Tests whether X register is the same
-    y_condition = (idy . registers $ mos6502) == (y cpustate) -- Tests whether Y register is the same
-    ps_condition = (ps . registers $ mos6502) == (p cpustate) -- Tests whether PS register is the same
-    condition =
-        pc_condition
+verifyCPU mos6502 cpustate = condition where
+    pc_condition  = (Test.CPU.pc cpustate) == (Shrimp.MOS6502.pc  . registers $ mos6502) 
+    sp_condition  = (Test.CPU.s  cpustate) == (sp  . registers $ mos6502)
+    acc_condition = (Test.CPU.a  cpustate) == (acc . registers $ mos6502)
+    x_condition   = (Test.CPU.x  cpustate) == (idx . registers $ mos6502)
+    y_condition   = (Test.CPU.y  cpustate) == (idy . registers $ mos6502) 
+    ps_condition  = (Test.CPU.p  cpustate) == (ps  . registers $ mos6502)
+    condition = pc_condition
             && sp_condition
             && acc_condition
             && x_condition
             && y_condition
             && ps_condition
 
-verify :: Barebones -> CPUState -> Bool
-verify barebones final_state = (verifyRam (bRam barebones) (ram final_state)) && (verifyCPU (bCpu barebones) final_state)
+verify :: Barebones -> CPUState -> IO Bool
+verify barebones final_state = do
+    vram <- verifyRam (bRam barebones) (ram final_state)
+    let vcpu = verifyCPU (bCpu barebones) final_state
+    return $ vram && vcpu
